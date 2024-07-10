ffac64d3a28ba0dd092f37ccb7886cb2a10c071d
Silvanos Eric <ericoderito@gmail.com>
2024-07-10 08:55:50 +0300

    feat(index.js): add higher-order functions
    - Add `receivesAFunction` to execute a callback function.
    - Implement `returnsANamedFunction` to return a named function.
    - Implement `returnsAnAnonymousFunction` to return an anonymous function.

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(cb) {
+  cb();
+}
+
+function returnsANamedFunction() {
+  return function namedFunction() {};
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {};
+}
